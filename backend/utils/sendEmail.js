2e20b29a0bb46f919b95dd2a63477d18858a6c90
ramya <ramyaluhar69@gmail.com>
2026-08-13 15:37:39 +0530

    Restore Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,19 @@
 // utils/sendEmail.js
-// Sends emails using Resend HTTPS API.
+// Sends OTP emails using Gmail SMTP.
 
-const { Resend } = require('resend');
+const nodemailer = require('nodemailer');
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendEmail = async ({ to, subject, text, html }) => {
-  await resend.emails.send({
-    from: 'StudentGenie <onboarding@resend.dev>',
+  await transporter.sendMail({
+    from: `"AI Student Assistant" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
